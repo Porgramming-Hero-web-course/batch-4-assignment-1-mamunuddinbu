7e6c7b0ce7b1c1af040511aa69abc1d571f9de8c
Md. Mamun Uddin <mamun.bbn.bd@gmail.com>
2024-11-08 12:35:19 +0600

    problem-1 solved

---
+++ src/problem-1.ts
@@ -0,0 +1,8 @@
+{
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+// Sample Input
+console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+}
