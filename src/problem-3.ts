9c50a6a5e5db4eeda04137172c4d5a33bc31a405
Md. Mamun Uddin <mamun.bbn.bd@gmail.com>
2024-11-08 12:50:54 +0600

    problem-3 solved

---
+++ src/problem-3.ts
@@ -0,0 +1,12 @@
+{
+    function countWordOccurrences(sentence: string, word: string): number {
+        const wordsArray = sentence.toLowerCase().split(" ");
+        const targetWord = word.toLowerCase();
+        
+        return wordsArray.filter(w => w === targetWord).length;
+    }
+    
+    // Sample Input
+    console.log(countWordOccurrences("I love typescript", "typescript")); // Output: 1
+    
+}
